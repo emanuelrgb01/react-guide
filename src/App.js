980b10851f7550142ba6474fb8cf0c260c14d67e
Emanuel Reinaldo <80110729+emanuelrgb01@users.noreply.github.com>
2022-12-15 09:23:50 -0300

    Files: update course project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const expenses = [
+		{
+			id: "e1",
+			title: "Car Insurance 1",
+			amount: 299.47,
+			date: new Date(2021, 2, 3)
+		},
+		{
+			id: "e2",
+			title: "Car Insurance 2",
+			amount: 245.47,
+			date: new Date(2021, 2, 28)
+		},
+		{
+			id: "e3",
+			title: "Car Insurance  3",
+			amount: 289.47,
+			date: new Date(2021, 1, 12)
+		},
+		{
+			id: "e4",
+			title: "Car Insurance  4",
+			amount: 219.47,
+			date: new Date(2020, 8, 14)
+		}
+	];
+	return (
+		<div>
+			<h2>Let's get started!</h2>
+			<p>Apenas o Brasil é penta.</p>
+			<Expenses expenses={expenses}/>
+		</div>
+	);
 }
 
 export default App;
